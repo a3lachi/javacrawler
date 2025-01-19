02d279d0a3ba3f79f0454c04b0839e274a870897
a3laxi <mad.aalachi@gmail.com>
2025-01-19 20:19:41 +0100

    Google blocks requests from the webdriver

--- GoogleCrawler.java
+++ GoogleCrawler.java
@@ -1,22 +1,15 @@
-
+import java.util.List;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class GoogleCrawler {
 
-    private WebDriver driver ;
-
 
     public static void main(String[] args) {
-        System.out.println("Frabat habt rido");
-        System.setProperty("webdriver.gecko.driver", "/Users/farawa/Downloads/geckodriver");
+        // System.setProperty("webdriver.gecko.driver", "/Users/farawa/Downloads/geckodriver");
 
         WebDriver driver = new FirefoxDriver();
 
@@ -24,8 +17,43 @@ public class GoogleCrawler {
             driver.get("https://www.google.com");
             System.out.println("Page Title: " + driver.getTitle());
         } catch (Exception e) {
+            System.out.println("Couldn't get the webdriver");
             e.printStackTrace();
         }
+
+        WebElement textArea = null;
+        try {
+            textArea = driver.findElement(By.tagName("textarea"));    
+        } catch (Exception e) {
+            System.out.println("Couldn't find the textarea element");
+        } finally {
+            textArea.sendKeys("inurl:\"cgi-bin/koha\"");
+            textArea.sendKeys(Keys.RETURN);
+            // WebElement search = driver.findElement(By.id("search_icon"));
+            // try {
+            //     WebElement svg = search.findElement(By.tagName("svg"));
+            //     textArea.sendKeys(Keys.ENTER);
+            //     textArea.sendKeys(Keys.ENTER);
+            //     textArea.sendKeys(Keys.ENTER);
+            // } catch (Exception e) {
+            //     System.out.println("Couldn't press the search button");
+            // }
+            
+        }
+
+        WebElement results = null ;
+
+        try {
+            results = driver.findElement(By.id("rso"));
+        } catch (Exception e) {
+            System.out.println("Couldn't find the results of the search") ;
+        } finally {
+            List<WebElement> divs = results.findElements(By.tagName("div"));
+            System.out.println("Number of <div> elements found: " + divs.size());
+        }
+
+
+
+    
     }
 }
-
